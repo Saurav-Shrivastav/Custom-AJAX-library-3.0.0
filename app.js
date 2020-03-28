7afc97814bc02ee70b2debba48b0a008343ed21d
Saurav <sauravsrivastav103@gmail.com>
2020-03-28 15:40:33 +0530

    POST method now functional.

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const http = new EasyHTTP;
 http.get('https://jsonplaceholder.typicode.com/users')
   .then(data => console.log(data))
   .catch(err => console.log(err));
+
+  // Create user data:
+  data = {
+    name: 'Saurav Shrivastav',
+    email: 'ss123@gmail.com'
+  }
+
+  // POST data:
+  http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
--- easyHTTPlib.js
+++ easyHTTPlib.js
@@ -16,5 +16,21 @@ class EasyHTTP {
     const resData = await response.json();
     return resData;
   }
-  
+
+  // Make a HTTP POST request:
+  async post(url, data) {
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    const resData = await response.json();
+    return resData;
+
+  }
+
 };
